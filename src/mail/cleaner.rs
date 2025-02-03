9c1c84934ce51622c8fad7d0f841867245418488
Dani Nugraha <dani.hmng@gmail.com>
2025-02-03 12:32:30 +0100

    disabled deleting files on debug mode

--- src/mail/cleaner.rs
+++ src/mail/cleaner.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+#[cfg(not(debug_assertions))]
 use tokio::fs;
 
 use super::Mail;
@@ -7,8 +9,20 @@ pub async fn remove_emails(mails: Vec<Mail>) -> Result<(), Box<dyn std::error::E
 		if !mail.file_path.exists() {
 			continue;
 		}
-		fs::remove_file(mail.file_path).await?;
+		remove_file(&mail.file_path).await?;
 	}
 
+	Ok(())
+}
+
+#[cfg(debug_assertions)]
+async fn remove_file(_: &Path) -> Result<(), Box<dyn std::error::Error>> {
+	println!("Not actually deleting file.");
+	Ok(())
+}
+
+#[cfg(not(debug_assertions))]
+async fn remove_file(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+	fs::remove_file(path).await?;
 	Ok(())
 }
