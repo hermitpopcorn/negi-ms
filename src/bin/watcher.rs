3ede1a7e9b3e5da3f12e2d6c4d394f99432d5caf
Dani Nugraha <dani.hmng@gmail.com>
2025-06-08 19:37:28 +0900

    change gemini model

--- src/bin/watcher.rs
+++ src/bin/watcher.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<(), ErrorInterface> {
 	dotenv().ok();
 	let parsers: Vec<Box<dyn EmailParsingScheme>> = vec![
 		Box::new(GeminiParsingScheme {
-			model: "gemini-2.0-flash",
+			model: "gemini-2.5-flash-preview-05-20",
 			accounts: vec!["Rakuten", "OCBC", "BCA", "Jenius"],
 			skips: vec![
 				"デイリーヤマザキアプ",
